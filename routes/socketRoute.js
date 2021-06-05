b069ae122f290b9e3ad2a36c56538b7d7f18dc5a
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2021-06-05 23:56:41 +0000

    added new socket function

--- routes/socketRoute.js
+++ routes/socketRoute.js
@@ -1,8 +1,8 @@
 import Post from "../models/PostModel.js";
 export const socketRoute = (io) => {
   io.on("connection", async (socket) => {
-    console.log("User connected");
-    socket.emit("kepler", { message: "pipe" });
+    const posts = await Post.find();
+    io.emit("getPosts", posts);
 
     socket.on("createPost", async (post) => {
       const { userId, time, caption, postImage, postImage_filename } = post;
@@ -14,7 +14,7 @@ export const socketRoute = (io) => {
         image_filename: postImage_filename,
       });
       const newPost = await createPost.save();
-      socket.emit("createPostSuccess", newPost);
+      io.emit("createPostSuccess", newPost);
     });
   });
 };
